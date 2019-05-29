d77ad1a712582d8b2b1220c0c1a8d11c47b3031c
megaman2528 <megaman2528@gmail.com>
2019-05-29 18:37:08 +0000

    correted some errors

--- Shopping app/src/com/shopingaapp/www/Auth.java
+++ Shopping app/src/com/shopingaapp/www/Auth.java
@@ -9,44 +9,63 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class Auth {
-	
+
 	private String line;
 
 	public void optionver(int optiontype) {
-		Scanner  inputScanner = new Scanner(System.in);
-		if(optiontype == 1) {
-			System.out.println("welcome to login");
-			
-			System.out.println("Please enter user name:\n");
+		Scanner inputScanner = new Scanner(System.in);
+		if (optiontype == 1) {
+			System.out.println("login");
+
+			System.out.println("Please enter user name:");
 			String username = inputScanner.nextLine();
-			System.out.println("Please enter your password:\n");
+			System.out.println("Please enter your password:");
 			String userpass = inputScanner.nextLine();
-			
-			File customertxt = new File("customer.txt");
-			
-			
-			
-			
+
+			File customertxt = new File("/home/freeze/git/Shoppingapp/Shopping app/customer.txt");
+
 			try {
 				FileReader customerReader = new FileReader(customertxt);
 				BufferedReader txtReader = new BufferedReader(customerReader);
-				ArrayList<String> datastore = new ArrayList<String>(Arrays.asList(line.split("\\s*,\\s*")));
-				while((line = txtReader.readLine()) != null);
-				datastore.add(line);
-				System.out.println(datastore);
+
+				ArrayList<String> datastore = new ArrayList<String>();
+
+				while ((line = txtReader.readLine()) != null) {
+
+					// datastore = Arrays.asList(lines);
+					datastore.addAll(Arrays.asList(line.split("\\s*,\\s*")));
+					// System.out.println(datastore);
+					// System.out.println(line);
 				
+
+			
 				
-			} catch ( IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			}
+			boolean uname = datastore.contains(username);
+			if (uname) {
+				System.out.println("You have logged in"+username);
+				
+			} else {
+				System.out.println("authication error:check your user name and password");
+
+			}
 			}
 			
+		 catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();}
+			
+			
+		 
+			
+			
+
+		}
+		else {
+			System.out.println("Admin login");
 			
 		}
 		
 	}
-	
-
 }
