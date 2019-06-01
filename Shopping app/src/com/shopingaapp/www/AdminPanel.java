8a9f937df45498b8f2805340040f13f9e7bb64e7
megaman2528 <megaman2528@gmail.com>
2019-06-01 22:50:30 +0000

    just some work

--- Shopping app/src/com/shopingaapp/www/AdminPanel.java
+++ Shopping app/src/com/shopingaapp/www/AdminPanel.java
@@ -1,5 +1,17 @@
 package com.shopingaapp.www;
 
-public class AdminPanel {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class AdminPanel {
+	public void write() {
+	File iFile = new File("Iteams.txt");
+	try {
+		FileWriter iFileWriter = new FileWriter(iFile);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	}
 }
